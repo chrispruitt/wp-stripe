112c071f37e500ba3b61078d9f7454f6ab992bce
Chris Pruitt <chris.pruitt15@gmail.com>
2015-02-25 21:44:49 -0600

    Adding Stripe lib, intitial wrapper class, and plugin main php.

--- wp-stripe.php
+++ wp-stripe.php
@@ -3,11 +3,10 @@
  * Plugin Name: wp-stripe
  * Plugin URI: https://github.com/chrispruitt/wp-stripe
  * Description: A brief description of the plugin.
- * Version: The plugin's version number. Example: 1.0.0
- * Author: Name of the plugin author
+ * Version: 1.0.0
+ * Author: Chris Pruitt so far
  * Author URI: http://URI_Of_The_Plugin_Author
- * Text Domain: Optional. Plugin's text domain for localization. Example: mytextdomain
- * Domain Path: Optional. Plugin's relative directory path to .mo files. Example: /locale/
- * Network: Optional. Whether the plugin can only be activated network wide. Example: true
  * License: A short license name. Example: GPL2
  */
+
+require_once('wp-wrapper/WPStripe.php');
---
+++ wp-wrapper/WPStripe.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once('../stripe/lib/Stripe.php');
+
+class WPStripe {
+    function __construct() {
+        $stripe = array(
+            'secret_key'      => 'sk_test_COkxTNWdVFrfHcwJtqp6LBid',
+            'publishable_key' => 'pk_test_fpJbqfr3gzCWFeSJaTBL6uhu'
+        );
+
+        Stripe::setApiKey($stripe['secret_key']);
+    }
+
+    function create_customer($user_id, $email = "", $description = "") {
+        $stripe_cus_id = get_user_meta($user_id, 'stripe_cus_id', true);
+        if( !empty ($stripe_cus_id)) {
+            $customer = Stripe_Customer::retrieve($stripe_cus_id);
+        } else {
+            $customer = Stripe_Customer::create(array(
+                "description" => $description,
+                "email" => $email
+            ));
+            update_user_meta( get_current_user_id(), 'stripe_cus_id', $customer['id']);
+        }
+        return $customer;
+    }
+
+    function delete_customer($user_id) {
+        $customer = Stripe_Customer::retrieve(get_user_meta($user_id, 'stripe_cus_id', true));
+        $customer->delete();
+        delete_usermeta(get_current_user_id(), 'stripe_cus_id');
+    }
+
+    function get_customer($user_id) {
+        $customer = Stripe_Customer::retrieve(get_user_meta($user_id, 'stripe_cus_id', true));
+        return $customer;
+    }
+
+    function update_payment_info($user_id, $card) {
+        $customer = $this->get_customer($user_id);
+        $customer->card = $card;
+        $customer->save();
+    }
+}
