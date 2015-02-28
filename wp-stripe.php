b65dd629e7c7e9f9a1b8c8c1e76ebdeb87210a54
Chris Pruitt <chris.pruitt15@gmail.com>
2015-02-28 17:56:00 -0600

    Make WPStripe static.

--- wp-stripe.php
+++ wp-stripe.php
@@ -13,6 +13,29 @@ require_once('wp-wrapper/WPStripe.php');
 require_once('stripe/lib/Stripe.php');
 require_once('SettingsPage.php');
 
+set_stripe_keys();
+
 if( is_admin() )
     $my_settings_page = new SettingsPage();
+
+
+function set_stripe_keys() {
+    $options = get_option('my_option_name');
+
+    if ($options['key_to_use'] == 'live') {
+        $stripe = array(
+            'secret_key'      => $options['live_secret_key'],
+            'publishable_key' => $options['live_publishable_key']
+        );
+    } else {
+        $stripe = array(
+            'secret_key'      => $options['test_secret_key'],
+            'publishable_key' => $options['test_publishable_key']
+        );
+    }
+
+    if (isset( $stripe['secret_key'] )) {
+        Stripe::setApiKey($stripe['secret_key']);
+    }
+}
 
--- wp-wrapper/WPStripe.php
+++ wp-wrapper/WPStripe.php
@@ -1,25 +1,8 @@
 <?php
 
 class WPStripe {
-    function __construct() {
-        $options = get_option('my_option_name');
 
-        if ($options['key_to_use'] == 'live') {
-            $stripe = array(
-                'secret_key'      => $options['live_secret_key'],
-                'publishable_key' => $options['live_publishable_key']
-            );
-        } else {
-            $stripe = array(
-                'secret_key'      => $options['test_secret_key'],
-                'publishable_key' => $options['test_publishable_key']
-            );
-        }
-
-        Stripe::setApiKey($stripe['secret_key']);
-    }
-
-    function create_customer($user_id, $email = "", $description = "") {
+    public static function create_customer($user_id, $email = "", $description = "") {
         $stripe_cus_id = get_user_meta($user_id, 'stripe_cus_id', true);
         if( !empty ($stripe_cus_id)) {
             $customer = Stripe_Customer::retrieve($stripe_cus_id);
@@ -33,19 +16,19 @@ class WPStripe {
         return $customer;
     }
 
-    function delete_customer($user_id) {
+    public static function delete_customer($user_id) {
         $customer = Stripe_Customer::retrieve(get_user_meta($user_id, 'stripe_cus_id', true));
         $customer->delete();
         delete_usermeta(get_current_user_id(), 'stripe_cus_id');
     }
 
-    function get_customer($user_id) {
+    public static function get_customer($user_id) {
         $customer = Stripe_Customer::retrieve(get_user_meta($user_id, 'stripe_cus_id', true));
         return $customer;
     }
 
-    function update_payment_info($user_id, $card) {
-        $customer = $this->get_customer($user_id);
+    public static function update_payment_info($user_id, $card) {
+        $customer = get_customer($user_id);
         $customer->card = $card;
         $customer->save();
     }
